8523635f41842c4ba07c9b9cd78a7725b85c05a0
Avi Craimer <mailavi@gmail.com>
2020-03-08 22:12:29 -0400

    Starting again to implement partial cat theory sandbox in Haskell.

--- category-theory/haskell/my-project/app/Ideation/Sketches/Sketch01.hs
+++ category-theory/haskell/my-project/app/Ideation/Sketches/Sketch01.hs
@@ -14,3 +14,35 @@ data Department p j e = Department Description  [Employee p j e]
 
 
 
+module Ideation.Sketches.Sketch01 ()  where
+    import qualified Data.Set
+    import qualified Data.Map
+
+    type UniqueId = String
+
+    data Object a = Ob UniqueId a
+
+    instance Show Object where
+        show Ob id _ = id
+
+
+    data Morphism a =  Morpism  UniqueId  (Object a --> Object a)
+
+    instance Show Morphism where
+        show Morphism  id  (obj1 --> obj2)  = show obj1 ++ "-" ++ id ++ "->" ++ show obj2
+
+
+-- If the string version is equal the morphisms are equal since it is assuemd the objects have a unique id
+    instance Eq Morphism where
+        m1 == m2 = show m1 == show m2
+
+
+    type Path = [Morphism] -- List of morphisms that are compatible to matchup tip to tail. In order from left to right.  [a-->b, b-->c]
+
+    type PathEquivalence =  Data.Set.Set Path   --A set of paths that are equivalent. They must match in their start and end points.
+
+    type MorphismString = String
+
+
+    data Category a  = Category (Data.Set.Set (Object  a) )  (Data.Map.Map Morphism PathEquivalence)
+
