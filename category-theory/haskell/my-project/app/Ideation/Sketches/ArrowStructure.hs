d465ee4ed814ddf7844bc5fcd40d1e1e3d2a5889
AVI CRAIMER <AVI.CRAIMER@cbc.ca>
2020-05-21 17:03:23 -0400

    Haskell style types in Javascript and other changes.

---
+++ category-theory/haskell/my-project/app/Ideation/Sketches/ArrowStructure.hs
@@ -0,0 +1,9 @@
+module Ideation.Sketches.ArrowStructure  () where
+
+
+
+-- data Arrow label = Self label | Arrow label (Arrow label)  (Arrow label) -- Source Target
+
+-- data ArrowStructure label = EmptyArrowStruct |
+
+data Arrow label = Self label
--- category-theory/haskell/my-project/app/Main.hs
+++ category-theory/haskell/my-project/app/Main.hs
@@ -3,14 +3,15 @@ module Main where
 -- import CatProgCourse.Lecture05.Svg
 -- import CatProgCourse.Lecture05.DataBasics
 -- import CatProgCourse.Lecture05.BinaryTree
--- import CatProgCourse.Lecture05.MakingFunctors
-import CatProgCourse.Lecture06.Products
-import CatProgCourse.Lecture07.Coproducts
+import CatProgCourse.Lecture05.MakingFunctors
+-- import CatProgCourse.Lecture06.Products
+-- import CatProgCourse.Lecture07.Coproducts
 -- import CatProgCourse.Lecture07.ExponentialObjs
 -- import CatProgCourse.Lecture07.Pullbacks
 -- import CatProgCourse.Lecture09.RecursiveDataTypes
 -- import CatProgCourse.Lecture11.FixCataAlgebra
 -- import BartoszCourse.Part1.Lecture05.SumTypes
+import Ideation.Sketches.ArrowStructure
 import Lib
 
 --To run in the REPL
